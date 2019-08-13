53a9a89ce046a829502e5b92ad959abc3e008b00
shiva banihashem <sbanihas@gmail.com>
2019-08-13 17:56:07 -0500

    Objectives met

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,36 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(attr) {
+  this.createdAt = attr.createdAt;
+  this.name = attr.name;
+  this.dimensions = attr.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  console.log(`${this.name} was removed from the game.`);
+};
+
+
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attr) {
+  GameObject.call(this, attr);
+  this.healthPoints = attr.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function() {
+  console.log(`${this.name} took damage.`);
+};
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +56,22 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(attr) {
+  CharacterStats.call(this, attr);
+  this.team = attr.team;
+  this.weapons = attr.weapons;
+  this.language = attr.language;
+};
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+console.log(Humanoid);
+
+Humanoid.prototype.greet = function() {
+  return (this.name + ' ' + 'offers a greeting in ' + this.language);
+};
+
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,9 +81,9 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
-    createdAt: new Date(),
+    createdAt: new Date() ,
     dimensions: {
       length: 2,
       width: 1,
@@ -57,6 +97,9 @@
     ],
     language: 'Common Tongue',
   });
+
+
+mage.greet(); // Lilith offers a greeting in Elvish.
 
   const swordsman = new Humanoid({
     createdAt: new Date(),
@@ -99,10 +142,10 @@
   console.log(swordsman.team); // The Round Table
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When a function is contained in the global scope, the value of this inside the function is going to be 
+the window/Console object.
+
+* 2. When a function is called by a preceding dot, this is going to refer to the object to the left of the dot.
+
+* 3. When a constructor function is used, this refers to an instance of the object that is created by
+ the constructor function by using the keyword new.
+
+* 4. When the JavaScript’s call or apply method is used, this is explicitly being definded by passing the objectname.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +18,56 @@
 
 // code example for Window Binding
 
+function globalFunction() {
+    console.log(this);
+  }
+  
+  globalFunction();
+
 // Principle 2
 
 // code example for Implicit Binding
 
+const implicitObj = {
+    type:"Error Code",
+    msg: function(name,msg) {
+      console.log(`${this.type} for ${name} is: ${msg}`);
+      console.log(this);
+    }
+  };
+  implicitObj.msg("Shiva","ReEnter your userid/password");
+
 // Principle 3
 
 // code example for New Binding
+function Userid(name) {
+    this.msg = 'your Userid is: ';
+    this.name = name;
+    this.display = function() {
+        console.log(`Hello, ${this.msg}${this.name.toLowerCase()}!`);
+        console.log(this);
+    }
+};
+
+const useridShiva = new Userid('Shiva');
+const useridSheena = new Userid('Sheena');
+
+useridShiva.display();
+useridSheena.display();
 
 // Principle 4
 
 // code example for Explicit Binding
+function Userid() {
+    this.msg = 'your Userid is: ';
+};
+const shiva = new Userid();
+
+let display = function(name,lname) {
+    console.log(`Hello, ${name} ${lname} ${this.msg}${name.toLowerCase()}!`);
+    console.log(this);
+}
+
+let nameArg =["Shiva","Banihasehm"];
+console.log("Output using .apply() below ");
+console.log(display.apply(shiva,nameArg ));
